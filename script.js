a34eda5509eb360ee645c54643fc7bc10e07a01c
shan18u <shivanshchhabra18@gmail.com>
2022-12-10 14:13:35 -0800

    update toggle.

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 $(document).ready(function(){
-
+    
     //Apply Dark theme
        $('body').toggleClass('dark-theme');
 
     $('#menu').click(function(){
+        $(this).toggleClass('fa-times');
         $('.navbar').toggleClass('nav-toggle');
     });
 })
