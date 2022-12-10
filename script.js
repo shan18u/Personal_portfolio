2571bb470968745184ec2caf7ccc1b5f2c248893
shan18u <shivanshchhabra18@gmail.com>
2022-12-10 14:13:07 -0800

    Apply dark theme

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 $(document).ready(function(){
-      
+
+    //Apply Dark theme
+       $('body').toggleClass('dark-theme');
 
     $('#menu').click(function(){
-       
         $('.navbar').toggleClass('nav-toggle');
     });
 })
