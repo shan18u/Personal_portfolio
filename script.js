8d0dd4faaf56108aa7d8cccaf0d702da97bbaea6
shan18u <shivanshchhabra18@gmail.com>
2022-12-13 20:54:28 -0800

    add image-container.

--- script.js
+++ script.js
@@ -12,5 +12,17 @@ $(window).on('scroll load',function(){
     $('#menu').removeClass('fa-times');
     $('.navbar').removeClass('nav-toggle');
   });
+  $('.portfolio .button-container .btn').click(function(){
+
+    let filter = $(this).attr('data-filter');
+
+    if(filter == 'all'){
+      $('.portfolio .image-container .box').show('400')
+    }else{
+      $('.portfolio .image-container .box').not('.'+filter).hide('200');
+      $('.portfolio .image-container .box').filter('.'+filter).show('400');
+    }
+
+  });
 
  
